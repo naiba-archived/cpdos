4f6e1ba99a877c90f9957942a7be44a9af0b4173
奶爸 <hi@nai.ba>
2019-10-30 23:11:17 +0800

    Simple UI

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CPDoS
+
 CPDoS: Cache Poisoned Denial of Service exp
+
+![捕获.PNG](https://i.loli.net/2019/10/30/UWd9HcYrnPg1tiJ.png)
--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -5,8 +5,20 @@ import (
 	_ "github.com/andlabs/ui/winmanifest"
 )
 
+var (
+	methods []string
+)
+
+func init() {
+	methods = []string{
+		"HTTP Header Oversize (HHO)",
+		"HTTP Meta Character (HMC)",
+		"HTTP Method Override (HMO)",
+	}
+}
+
 func setupUI() {
-	mainwin := ui.NewWindow("libui Text-Drawing Example", 640, 480, true)
+	mainwin := ui.NewWindow("CPDoS Test Tool", 300, 100, true)
 	mainwin.SetMargined(true)
 	mainwin.OnClosing(func(*ui.Window) bool {
 		mainwin.Destroy()
@@ -18,9 +30,35 @@ func setupUI() {
 		return true
 	})
 
-	hbox := ui.NewHorizontalBox()
-	hbox.SetPadded(true)
-	mainwin.SetChild(hbox)
+	vbox := ui.NewVerticalBox()
+	vbox.SetPadded(true)
+	mainwin.SetChild(vbox)
+
+	urlBox := ui.NewHorizontalBox()
+	urlBox.SetPadded(true)
+	lbURL := ui.NewLabel("Website URL")
+	etURL := ui.NewEntry()
+	urlBox.Append(lbURL, false)
+	urlBox.Append(etURL, true)
+	vbox.Append(urlBox, false)
+
+	methodBox := ui.NewHorizontalBox()
+	methodBox.SetPadded(true)
+	lbMethod := ui.NewLabel("CPDoS Type")
+	cbMethod := ui.NewCombobox()
+	for i := 0; i < len(methods); i++ {
+		cbMethod.Append(methods[i])
+	}
+	cbMethod.SetSelected(0)
+	methodBox.Append(lbMethod, false)
+	methodBox.Append(cbMethod, true)
+	vbox.Append(methodBox, false)
+
+	btnVerify := ui.NewButton("Verify")
+	btnVerify.OnClicked(func(btn *ui.Button) {
+		ui.MsgBox(mainwin, "Hola", "You clicked the button!")
+	})
+	vbox.Append(btnVerify, false)
 
 	mainwin.Show()
 }
